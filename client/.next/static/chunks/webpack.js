39c696e63d1b46e2f8a96ef3a09dcd6112ae9ea1
Hanan501 <hanan11.mohsin@gmail.com>
2024-06-28 15:00:15 +0300

    issue:Implement the About us page

--- client/.next/static/chunks/webpack.js
+++ client/.next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "fc93b04d93aa52f7"; }
+/******/ 		__webpack_require__.h = function() { return "7317f8f4f577e34e"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
